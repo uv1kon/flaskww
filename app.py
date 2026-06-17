8cad45296a436e89c754a295b1df8169f1623b1d
Victor <ddarho52@gmail.com>
2026-06-17 06:02:00 +0500

    День 1

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -25,9 +25,24 @@ def index():
 def about():
     return render_template('about.html')
 
-@app.route('/user/<string:name>/<int:id>')
-def user(name, id):
-    return "user page: " + name + " -" +str(id)
+@app.route('/create-article', methods=['POST', 'GET'])
+def create_article():
+    if request.method == 'POST':
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+
+        article = Article(title=title, intro=intro, text=text)
+
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "При добавлении статьи произошла ошибка"
+    else:
+        return render_template('create-article.html')
+
 
 
 if __name__ == '__main__':
